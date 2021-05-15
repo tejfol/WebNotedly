00adaf17facd633dd666aa4094a7446fe2da2cbe
tejfol <tejfolcss@gmail.com>
2021-05-15 16:25:42 +0300

    Sing in and out

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,63 @@
+// import React and our routing dependencies
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
+// import our shared layout component
 import Layout from '../components/Layout';
 
+// import our routes
 import Home from './home';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
-import NotePage from './note';
+import Note from './note';
 import SignUp from './signup';
+import SignIn from './signin';
 
-const Pages = () => {
+const IS_LOGGED_IN = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+// define our routes
+const Pages = props => {
   return (
     <Router>
       <Layout>
         <Route exact path="/" component={Home} />
-        <Route path="/mynotes" component={MyNotes} />
-        <Route path="/favorites" component={Favorites} />
-        <Route path="/note/:id" component={NotePage} />
+        <PrivateRoute path="/mynotes" component={MyNotes} />
+        <PrivateRoute path="/favorites" component={Favorites} />
+        <Route path="/note/:id" component={Note} />
         <Route path="/signup" component={SignUp} />
+        <Route path="/signin" component={SignIn} />
       </Layout>
     </Router>
+  );
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { loading, error, data } = useQuery(IS_LOGGED_IN);
+  // if the data is loading, display a loading message
+  if (loading) return <p>Loading...</p>;
+  // if there is an error fetching the data, display an error message
+  if (error) return <p>Error!</p>;
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        data.isLoggedIn === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/signin',
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
   );
 };
 
