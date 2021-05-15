4913d3f6c5927b249950b0124618b9fc83ea5e2b
tejfol <tejfolcss@gmail.com>
2021-05-15 09:20:40 +0300

    single page note view added

--- src/components/NoteFeed.js
+++ src/components/NoteFeed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Note from './Note.js';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const NoteWrapper = styled.div`
   max-width: 800px;
@@ -16,6 +17,7 @@ const NoteFeed = ({ notes }) => {
       {notes.map(note => (
         <NoteWrapper key={note.id}>
           <Note note={note} />
+          <Link to={`note/${note.id}`}>Permalink</Link>
         </NoteWrapper>
       ))}
     </div>
